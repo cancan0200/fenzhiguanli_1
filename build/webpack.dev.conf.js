2cebbddfa2bb9fa34e4e5b8d05eb4c1b0ce8aa91
czhou <2550936397@qq.com>
2019-10-28 10:41:23 +0800

    修改图标格式

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -56,7 +56,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.html',
       inject: true,
-      favicon: './static/icon-title.ico'
+      favicon: './static/icon-title.png'
     }),
     // copy custom static assets
     new CopyWebpackPlugin([
